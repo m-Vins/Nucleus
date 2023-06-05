1136d06c3450af7743e641148e4eedea19d9493c
vincenzo mezzela <mezzela@eurecom.fr>
2023-06-05 10:40:10 +0200

    minor fixes [compiling code now]

--- loader.cc
+++ loader.cc
@@ -353,9 +353,9 @@ int load_sections_bfd(bfd *bfd_h, Binary *bin)
       continue;
     }
 
-    vma = bfd_section_vma(bfd_h, bfd_sec);
-    size = bfd_section_size(bfd_h, bfd_sec);
-    secname = bfd_section_name(bfd_h, bfd_sec);
+    vma = bfd_section_vma(bfd_sec);
+    size = bfd_section_size(bfd_sec);
+    secname = bfd_section_name(bfd_sec);
     if (!secname)
       secname = "<unnamed>";
 
