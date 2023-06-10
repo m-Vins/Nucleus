1565d5fea78dbc1709fea73c5ae6273bfeda3270
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2023-06-10 16:36:26 +0200

    Fix makefile

--- test/cmp_symbols.sh
+++ test/cmp_symbols.sh
@@ -4,7 +4,7 @@ set -e  # Stop if there is a failure
 # Check if the user has provided an argument
 if [ $# -ne 1 ]; then
     echo -e "\n\033[1;35m\tbash cmp_symbols.sh <ELF file, not stripped>\033[0m"
-    echo -e "Compares the output of nucleus with the output of nm, adding symbol names to functions.\n"
+    echo -e "Compares the output of nucleus (run on the stripped file) with the output of nm, adding symbol names to functions.\n"
     exit
 fi
 
@@ -22,18 +22,23 @@ if [[ -n "$is_stripped" ]]; then
 fi
 
 nm_out=$(nm $elf_name)
+elf_stripped="${elf_name}_stripped"
+cp $elf_name $elf_stripped
+strip -s $elf_stripped
 
 # Iterate over each line in the output
 while IFS= read -r line; do
     # Extract the function number
     function_num=$(echo $line | sed -e 's,^.*x\(.*\) .* .*$,\1,g')
     # Extract the symbol name of the function
-    function_name=$(echo $nm_out | sed -e "s,^.*${function_num} . \([^ ]\+\).*$,\1,")
+    function_name=$(echo $nm_out | sed -e "s,^.*${function_num} \(.\) \([^ ]\+\).*$,\2 (\1),")
 
     word_count=$(echo "$function_name" | wc -w)
-    if [ "$word_count" -ne 1 ]; then
+    if [ "$word_count" -ne 2 ]; then
         echo -e "$line\t-->\033[1;31m symbol not found \033[0m"
     else
         echo -e "$line\t-->\033[1;32m $function_name \033[0m"
     fi
-done < <("${BASE_DIR}/nucleus" -e $elf_name -d linear | grep function)
+done < <("${BASE_DIR}/nucleus" -e $elf_stripped -d linear | grep function)
+
+rm $elf_stripped
