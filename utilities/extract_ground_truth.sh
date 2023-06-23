adc5936589cf07ee87c07a9a0212c1d381b40825
vincenzo mezzela <mezzela@eurecom.fr>
2023-06-23 11:07:26 +0200

    updated extract_ground_truth.sh

--- utilities/extract_ground_truth.sh
+++ utilities/extract_ground_truth.sh
@@ -2,6 +2,11 @@
 
 file_in="./../binary_function_similarity/DBs/Dataset-1/training_Dataset-1.csv"
 target_path="./test/ground_truth"
+# binaries_list is a file containing the names of all the binaries extracted, it is useful to
+# fetch the binaries we need from the database
+binaries_list="./test/binaries_list.txt"
+# clear the file
+echo "" > $binaries_list
 
 
 # Extract available archs
@@ -13,19 +18,28 @@ for arch in $archs; do
     echo "Creating folder: $folder_path"
     mkdir -p $folder_path
     
-    
-    # extract executable names for the current bin
-    binaries=$(cat $file_in | awk -F, '{print $2}' | awk -F/ '{print $4}' | grep $arch | sort | uniq)
-    
-    for bin in $binaries; do
+    while IFS= read -r line; do
+        program=$(echo "$line" | cut -d " " -f 2)
+        bin=$(echo "$line" | cut -d " " -f 1)
+ 
+        # removing ".i64" from the name
         file_out=$(basename "$bin" .i64)
+
+        # write the path of the binary in binaries_list in order 
+        # to fetch them afterwards using the script copy_binaries.sh
+        echo "$program/$file_out" >> $binaries_list
+
         file_out="$target_path/$arch/$file_out"
+
+        # Check if the file already exists
         if [ -e "$file_out" ]; then
             echo "File $file_out already exists."
         else      
-            # extracting info for the current bin
+            # extracting info for the current bin following the structure:
+            # function_name start_address
             echo "Extracting info for $bin"
             cat $file_in | grep $bin | awk -F, '{print $4,$3}' > $file_out
         fi
-    done
+
+    done <<< "$(cat $file_in | awk -F, '{print $2}' | awk -F/ '{print $4,$3}' | grep $arch | sort | uniq)"
 done
