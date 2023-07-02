9f18746670daa43396691c33ab213de5906e6c93
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2023-07-02 14:55:52 +0200

    Fix missing files

--- test/scripts/extract_text_size.sh
+++ test/scripts/extract_text_size.sh
@@ -8,5 +8,10 @@ touch "${BASE_DIR}/test/text_sizes.txt"
 
 for binary in "${BASE_DIR}/test/binaries"/*; do
     # Use readelf to get the size of the code section
-    readelf -SW $binary | awk '$2 == ".text" { printf "%d\n", "0x" $6}' >> "${BASE_DIR}/test/text_sizes.txt"
+    out=$(readelf -SW $binary | awk '$2 == ".text" { printf "%d\n", "0x" $6}')
+    if [ -z "$out" ]; then
+        # It is empty
+        out=$(readelf -SW $binary | awk '$3 == ".text" { printf "%d\n", "0x" $7}')
+    fi
+    echo $out >> "${BASE_DIR}/test/text_sizes.txt"
 done
